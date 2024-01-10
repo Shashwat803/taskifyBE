39e5e6de47580c97612b348ce8739e5775d39db4
ExerticaShashwat <exertica.shashwat@outlook.com>
2024-01-10 12:22:57 +0530

    updared Cors with new options

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ const cors = require('cors')
 
 connectDb()
 
+const corsOptions = {
+    origin: 'https://taskify-fe-ruby.vercel.app',
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  };
+  
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.use(cors())
+app.use(cors(corsOptions))
 
 
 app.use('/api/todos', require('./routes/todoRoutes'))
